19aea70c5143b9550c80057778c3b3403444d1ac
1u1ucas <lucasleveque17@gmail.com>
2024-10-10 14:12:52 +0200

    -initialise-

---
+++ order/view/partials/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Eshop au top</title>
+	</head>
+	<body>
+
+	<header>
+		<h1>Le Eshop au top</h1>
+
+		<nav>
+			<ul>
+				<li><a href="http://localhost:8888/projet-methodo-tp-1/">Commander</a></li>
+				<li><a href="http://localhost:8888/projet-methodo-tp-1/set-shipping-address">Renseigner l'adresse de livraion</a></li>
+				<li><a href="http://localhost:8888/projet-methodo-tp-1/set-shipping-method">Renseigner la méthode de livraison</a></li>
+				<li><a href="http://localhost:8888/projet-methodo-tp-1/pay">Payer</a></li>
+
+
+			</ul>
+		</nav>
+	</header>
